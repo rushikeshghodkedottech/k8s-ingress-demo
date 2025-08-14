5663833654601232228e19629180326ddb5a6f57
rghodke <rghodke@semtech.com>
2025-08-14 15:16:08 +0530

    update

--- commands.sh
+++ commands.sh
@@ -1,9 +1,13 @@
-kubectl apply -f deployment.yaml
+kubectl apply -f deployment.yaml -f service.yaml
 
-kubectl apply -f service.yaml
+kubectl apply -f ./web/deployment.yaml -f ./web/service.yaml
+
+kubectl apply -f ./welcome/deployment.yaml -f ./welcome/service.yaml
 
 kubectl get all
 
-kubectl port-forward service/nginx-service 80:80
+# kubectl port-forward service/nginx-service 80:80 -> for testing
 
+kubectl apply -f ingress.yaml
+# or
 kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
