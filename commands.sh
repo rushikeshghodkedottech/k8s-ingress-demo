12264aa9a40a38203ca58bed5938546615b0dcff
rushikesh <rushikeshexperimental@gmail.com>
2025-08-22 11:31:56 +0530

    Add files via upload

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
+kind create cluster --name k8s-cluster --config=create-cluster.yaml #create a k8s cluster
+
 kubectl apply -f deployment.yaml -f service.yaml
 
 kubectl apply -f ./web/deployment.yaml -f ./web/service.yaml
@@ -6,8 +8,16 @@ kubectl apply -f ./welcome/deployment.yaml -f ./welcome/service.yaml
 
 kubectl get all
 
-# kubectl port-forward service/nginx-service 80:80 -> for testing
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 kubectl apply -f ingress.yaml
-# or
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+# kubectl port-forward service/nginx-service 80:80 -> for testing
+
 kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
+
